00c48aad2c9e9e053a93da1d296b2b9f03066ad8
Daniel Bonhaure <danielbonhaure@gmail.com>
2023-03-27 17:28:15 -0400

    Mejora tiempo focal smoothing

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def custom_mean_smap(arr):
 
 
 @njit
-def focal(arr, size):
+def focal_1(arr, size):
     """Calculate the focal mean of `arr` using a window of `size`."""
     # Create an empty array to store the result
     result = np.empty_like(arr)
@@ -48,6 +48,27 @@ def focal(arr, size):
             subset = arr[start_i:end_i, start_j:end_j].flatten()
             # result[i, j] = custom_mean_smap(subset)
             if not np.isnan(subset[len(subset) // 2]):
+                result[i, j] = np.nanmean(subset)
+    return result
+
+
+@njit
+def focal_2(arr, size):
+    """Calculate the focal mean of `arr` using a window of `size`."""
+    # Create an empty array to store the result
+    result = np.empty_like(arr)
+    result[:] = np.nan
+    # Iterate over each pixel in the input array
+    for i in range(arr.shape[0]):
+        for j in range(arr.shape[1]):
+            # Compute the range of pixels to use for the focal operation
+            start_i = max(i - size // 2, 0)
+            end_i = min(i + size // 2 + 1, arr.shape[0])
+            start_j = max(j - size // 2, 0)
+            end_j = min(j + size // 2 + 1, arr.shape[1])
+            # Compute the mean of the subset of pixels
+            if not np.isnan(arr[i, j]):
+                subset = arr[start_i:end_i, start_j:end_j].flatten()
                 result[i, j] = np.nanmean(subset)
     return result
 
@@ -69,10 +90,11 @@ def plot_array_am(array_am, title='Título'):
 
 
 if __name__ == '__main__':
+    graficar = False
 
     # Leer archivos hdf5
     soil_moistures = []
-    for h5_file in os.listdir('test_files'):
+    for h5_file in [x for x in os.listdir('test_files') if x != ".gitkeep"]:
         data = h5py.File(f"test_files/{h5_file}", 'r')
         c_soil_moisture = np.array(data['Soil_Moisture_Retrieval_Data_AM']['soil_moisture'])  # Get data from h5_file
         c_soil_moisture[c_soil_moisture == -9999] = np.nan  # Asignan NaN a -9999
@@ -83,42 +105,49 @@ if __name__ == '__main__':
 
     # Calcular media y graficar
     print(f"media: {np.nanmean(soil_moisture)}")
-    plot_array_am(soil_moisture, "Datos Originales")
+    plot_array_am(soil_moisture, "Datos Originales") if graficar else None
 
     # Aplicar suavizado (similar a raster::focal)
     start = time.time()
     array_am_1_1 = generic_filter(soil_moisture, custom_mean_smap, size=5, mode='constant')
     end = time.time()
     print(f"tiempo: {end - start} -- media: {np.nanmean(array_am_1_1)} -- generic_filter con size")
-    plot_array_am(array_am_1_1, "generic_filter con size")
+    plot_array_am(array_am_1_1, "generic_filter con size") if graficar else None
 
     # Aplicar suavizado (similar a raster::focal)
     start = time.time()
     array_am_1_2 = generic_filter(soil_moisture, custom_mean_smap, footprint=np.ones((5, 5)), mode='constant')
     end = time.time()
     print(f"tiempo: {end - start} -- media: {np.nanmean(array_am_1_2)} -- generic_filter con footprint")
-    plot_array_am(array_am_1_2, "generic_filter con footprint")
+    plot_array_am(array_am_1_2, "generic_filter con footprint") if graficar else None
 
     # Aplicar suavizado (similar a raster::focal)
     start = time.time()
     array_am_1_3 = fasted_generic_filter(soil_moisture)
     end = time.time()
     print(f"tiempo: {end - start} -- media: {np.nanmean(array_am_1_3)} -- generic_filter IDIC")
-    plot_array_am(array_am_1_3, "generic_filter IDIC")
+    plot_array_am(array_am_1_3, "generic_filter IDIC") if graficar else None
 
     # Aplicar suavizado (similar a raster::focal)
     start = time.time()
-    array_am_2 = focal(soil_moisture, size=5)
+    array_am_2_1 = focal_1(soil_moisture, size=5)
     end = time.time()
-    print(f"tiempo: {end - start} -- media: {np.nanmean(array_am_2)} -- focal manual")
-    plot_array_am(array_am_2, "focal manual")
+    print(f"tiempo: {end - start} -- media: {np.nanmean(array_am_2_1)} -- focal manual 1")
+    plot_array_am(array_am_2_1, "focal manual 2") if graficar else None
+
+    # Aplicar suavizado (similar a raster::focal)
+    start = time.time()
+    array_am_2_2 = focal_2(soil_moisture, size=5)
+    end = time.time()
+    print(f"tiempo: {end - start} -- media: {np.nanmean(array_am_2_2)} -- focal manual 2")
+    plot_array_am(array_am_2_2, "focal manual 2") if graficar else None
 
     # Aplicar suavizado (similar a raster::focal)
     start = time.time()
     array_am_3 = median_filter(soil_moisture, size=5, mode='wrap')
     end = time.time()
     print(f"tiempo: {end - start} -- media: {np.nanmean(array_am_3)} -- median_filter")
-    plot_array_am(array_am_3, "median_filter")
+    plot_array_am(array_am_3, "median_filter") if graficar else None
 
     # Aplicar suavizado (similar a raster::focal)
     start = time.time()
@@ -128,7 +157,7 @@ if __name__ == '__main__':
     array_am_4 = convolve2d(soil_moisture, kernel, mode='same', boundary='wrap')
     end = time.time()
     print(f"tiempo: {end - start} -- media: {np.nanmean(array_am_4)} -- convolve2d")
-    plot_array_am(array_am_4, "convolve2d")
+    plot_array_am(array_am_4, "convolve2d") if graficar else None
 
     parar = True
 
